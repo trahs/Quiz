d7fdc41fbe50ecfabea98ece9b932aa959226c0a
trahs <162071971+trahs@users.noreply.github.com>
2024-03-05 11:20:35 +0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Siapa paling cantik?",
+        answer: "takde",
+        options: [
+            "takde",
+            "raze",
+            "eira",
+            "siti"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Warna pelangi?",
+        answer: "jingga",
+        options: [
+            "hitam",
+            "putih",
+            "jingga",
+            "coklat"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Ayam ada berapa kaki?",
+        answer: "dua",
+        options: [
+            "satu",
+            "dua",
+            "tiga",
+            "empat"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Lembu minum apa?",
+        answer: "air",
+        options: [
+            "susu",
+            "coke",
+            "sprite",
+            "air"
+        ]
+    },
+];
